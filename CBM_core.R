5c7dcd306cb3dfa9ea10078a6b83467300970f82
Susan Murray <murray.e.susan@gmail.com>
2026-03-31 13:44:07 -0700

    CBM4 version bump

--- CBM_core.R
+++ CBM_core.R
@@ -16,7 +16,7 @@ defineModule(sim, list(
   reqdPkgs = list(
     "data.table", "arrow", "dplyr",
     "PredictiveEcology/CBM4r@development",
-    "PredictiveEcology/CBMutils@development (>=2.5)"
+    "PredictiveEcology/CBMutils@development (>=2.5.3)"
   ),
   parameters = rbind(
     defineParameter("fixedCohorts", "logical", TRUE, NA, NA, "Stand cohorts are fixed for simulation duration"),
